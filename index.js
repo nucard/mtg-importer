835b21cb3caf4998731ab6ed119750a72ba92a08
ben <ben@jammerware.com>
2018-07-14 13:11:07 -0400

    Refactored loading logic into its own method

--- index.js
+++ index.js
@@ -39,61 +39,65 @@ function deleteAllCards() {
     })
 }
 
-function importCards() {
-    return new Promise((resolve, reject) => {
-        const cards = {};
+function loadCardData() {
+    const cards = {};
 
-        for (const setCode of _.keys(rawData)) {
-            const setCards = rawData[setCode].cards;
+    for (const setCode of _.keys(rawData)) {
+        const setCards = rawData[setCode].cards;
 
-            for (let setCard of setCards) {
-                let card = cards[setCard.name];
+        for (let setCard of setCards) {
+            let card = cards[setCard.name];
 
-                // if this isn't a card we've hit before, we need to add an entry
-                if (!card) {
-                    const newCard = {
-                        id: setCard.id,
-                        searchId: setCard.name.toLowerCase().replace(' ', ''), // for algolia search
-                        name: setCard.name,
-                        rarity: setCard.rarity,
-                        cost: setCard.manaCost || null,
-                        types: (setCard.supertypes || []).concat(setCard.types || []),
-                        subtypes: setCard.subtypes || [],
-                        text: setCard.text || null,
-                        thumbnail: setCard.multiverseid ?
-                            `http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=${setCard.multiverseid}&type=card` :
-                            null,
-                        printings: []
-                    };
-
-                    cards[setCard.name] = newCard;
-                    card = newCard;
-                }
-
-                // add the printing to the entry
-                card.printings.push({
-                    artist: setCard.artist,
-                    flavorText: setCard.flavor || null,
-                    image: setCard.multiverseid ?
+            // if this isn't a card we've hit before, we need to add an entry
+            if (!card) {
+                const newCard = {
+                    id: setCard.id,
+                    searchId: setCard.name.toLowerCase().replace(' ', ''), // for algolia search
+                    name: setCard.name,
+                    rarity: setCard.rarity,
+                    cost: setCard.manaCost || null,
+                    types: (setCard.supertypes || []).concat(setCard.types || []),
+                    subtypes: setCard.subtypes || [],
+                    text: setCard.text || null,
+                    thumbnail: setCard.multiverseid ?
                         `http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=${setCard.multiverseid}&type=card` :
                         null,
-                    printedIn: setCode
-                });
-            }
-        }
+                    printings: []
+                };
 
+                cards[setCard.name] = newCard;
+                card = newCard;
+            }
+
+            // add the printing to the entry
+            card.printings.push({
+                artist: setCard.artist,
+                flavorText: setCard.flavor || null,
+                image: setCard.multiverseid ?
+                    `http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=${setCard.multiverseid}&type=card` :
+                    null,
+                printedIn: setCode
+            });
+        }
+    }
+
+    console.log(`Loaded ${cards.length} cards from raw data...`);
+    return _.values(cards);
+}
+
+function importCards(cards) {
+    return new Promise((resolve, reject) => {
         const allBatches = [];
-        const cardValues = _.values(cards);
         let batch = db.batch();
         let batchCounter = 0;
 
-        console.log(`Adding ${cardValues.length} cards...`);
-        for (let i = 0; i < cardValues.length; i++) {
+        console.log(`Adding ${cards.length} cards...`);
+        for (let i = 0; i < cards.length; i++) {
             const cardRef = db.collection('cards').doc();
-            batch.set(cardRef, cardValues[i]);
+            batch.set(cardRef, cards[i]);
             batchCounter++;
 
-            if (batchCounter === 500 || i === cardValues.length - 1) {
+            if (batchCounter === 500 || i === cards.length - 1) {
                 allBatches.push(batch);
                 batch = db.batch();
                 batchCounter = 0;
@@ -105,21 +109,11 @@ function importCards() {
         }
 
         console.log('Done.');
-        resolve(cardValues);
+        resolve();
     });
 }
 
 async function createSearchIndex(cards) {
-    cards = cards || [];
-
-    // if we don't have the cards locally, go get them
-    if (!cards.length) {
-        const cardDocs = await getAllCards();
-        cardDocs.forEach(doc => {
-            cards.push(doc.data());
-        });
-    }
-
     // assign cards an objectID (note spelling) for algolia
     for (const card of cards) {
         card.objectID = card.name.toLowerCase().replace(' ', '');
@@ -130,7 +124,8 @@ async function createSearchIndex(cards) {
 }
 
 (async () => {
-    // await deleteAllCards();
-    // const cards = await importCards();
-    await createSearchIndex();
+    const cards = loadCardData();
+    await deleteAllCards();
+    await importCards(cards);
+    await createSearchIndex(cards);
 })();
